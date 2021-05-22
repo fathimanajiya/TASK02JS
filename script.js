875aebef349b569ac4f0bdd027447d33d77251a0
fathima <fathimanajiya212@gmail.com>
2021-05-22 00:24:28 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function tweet(){
-    
+document.getElementById("btn").addEventListener("click", function tweet(){
+
+
+    document.querySelector("name").value;
+    document.querySelector("email").value;
+    document.querySelector("text").value;
+
+let name  = document.createElement("p");
+let email = document.createElement("p");
+let text = document.createElement("p");
+
+p1.textContent =querySelector("name").value;
+p2.textContent = querySelector("email").value;
+p3.textContent = querySelector("text").value;
+
+const content = document.querySelector(".content");
+content.appendChild(p1);
+content.appendChild(p2);
+content.appendChild(p3);
 }
